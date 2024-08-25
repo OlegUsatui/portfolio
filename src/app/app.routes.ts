b44a610875eb0987831e955ea550ff05d2ad3a9b
oleg-usatyi <oleg_usatyi@ukr.net>
2024-08-25 19:57:07 +0200

    added files structures

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule) },
+  { path: 'portfolio', loadChildren: () => import('./features/portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: 'contacts', loadChildren: () => import('./features/contacts/contacts.module').then(m => m.ContactsModule) },
+  { path: 'recruiter', loadChildren: () => import('./features/recruiter/recruiter.module').then(m => m.RecruiterModule) },
+  { path: '**', redirectTo: '' }
+];
+
---
+++ src/app/core/index.ts
@@ -0,0 +1,6 @@
+export * from './services/auth.service';
+export * from './services/theme.service';
+export * from './services/localization.service';
+export * from './services/api.service';
+export * from './guards/auth.guard';
+export * from './interceptors/auth.interceptor';
